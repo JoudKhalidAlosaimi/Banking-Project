7b30297da7b250d41826030118a768c57c9e0b7a
Joud Alosaimi <joudalosaimi11@gmail,com>
2025-09-23 17:41:41 +0300

    testing the deposit method

--- tests/test_account.py
+++ tests/test_account.py
@@ -1,2 +1,12 @@
 import unittest
 from BankProject.account import Account
+from BankProject.account import AmountError
+
+class TestAccount(unittest.TestCase):
+
+    def setUp(self):
+        print('Setting up')
+        self.new_deposit = Account()
+
+    def test_account_deposit(self):
+        self.assertEqual(self.new_deposit.deposit('checkings', 22, '68da891c-f017-4ad6-a711-d28897ee7ae8'), 100944.0)
