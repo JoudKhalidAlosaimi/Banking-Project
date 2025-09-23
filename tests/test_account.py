4622f27cbee7fee76c23f3866760c9a9cfaea176
Joud Alosaimi <joudalosaimi11@gmail,com>
2025-09-23 17:42:55 +0300

    testing the deposit method, adding a tear down

--- tests/test_account.py
+++ tests/test_account.py
@@ -7,6 +7,9 @@ class TestAccount(unittest.TestCase):
     def setUp(self):
         print('Setting up')
         self.new_deposit = Account()
+    
+    def tearDown(self):
+        print('Tearing down')
 
     def test_account_deposit(self):
-        self.assertEqual(self.new_deposit.deposit('checkings', 22, '68da891c-f017-4ad6-a711-d28897ee7ae8'), 100944.0)
+        self.assertEqual(self.new_deposit.deposit('checkings', 22, '68da891c-f017-4ad6-a711-d28897ee7ae8'), 100966.0)
